c47ac10b641d684e5f5f058005ca83461135738a
JeffS1lva <jeffdsilva.29@gmail.com>
2025-03-21 10:31:18 -0300

    teste web config

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the React build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Serve the index.html for any request not found in static files
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
